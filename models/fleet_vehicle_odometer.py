07ef28be1c9848343bd3160e98f42d5cb95e55c9
Eduardo Vizcaino Granados <e.vizcaino@solesgps.com>
2023-03-05 21:50:13 -0600

    [ADD]gpsmap: Cron odometer

--- models/fleet_vehicle_odometer.py
+++ models/fleet_vehicle_odometer.py
@@ -1,8 +1,22 @@
 from odoo import fields, models
-import pytz
-
 
 class odometer(models.Model):
     _inherit = "fleet.vehicle.odometer"
     _order = "date ASC"
-    activeTime = fields.Float('Active Time', digits = (3, 2))
+    activeTime = fields.Float('Active Time / hr', digits = (3, 2))
+
+    def run_scheduler_set_odometer(self):
+        for vehicle in self.env['fleet.vehicle'].search([]):
+            
+            if(vehicle.odometer_unit=='miles'):
+                type_distance = 1609.34
+            else:
+                type_distance = 1000
+
+            self.create({
+                "vehicle_id": vehicle.id,
+                "value": int(vehicle.positionid.totalDistance) / type_distance,
+                "date": vehicle.positionid.devicetime,                
+                "activeTime": int(vehicle.active_time_today) / 60,
+            })
+            vehicle.active_time_today=0
