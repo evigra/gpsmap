5e40c2246e9f328569b5e8afe191cac872a533a7
Eduardo Vizcaino Granados <e.vizcaino@solesgps.com>
2023-10-06 06:15:53 -0600

    [ADD]gpsmap: Add the send_mail field in the alerts model

--- models/gps_alerts.py
+++ models/gps_alerts.py
@@ -10,3 +10,4 @@ class gps_alerts(models.Model):
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, required=True)
     vehicle_ids = fields.Many2many('fleet.vehicle', 'gps_alerts_vehicles_rel', 'vehicle_id', 'alert', string = 'Vehicles')
     geofence_ids = fields.Many2many('gps_geofences', 'gps_alerts_geofences_rel', 'geofence_id', 'alert', string = 'Geofences')
+    send_mail = fields.Boolean(default = False)
